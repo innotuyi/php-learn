d98eee0b9cf38c2518e8207a0c9db9de6d7ef49b
root <innotuyi.com>
2022-04-25 15:28:11 +0200

    Added array file

---
+++ array.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$programming = ['c++', 'java', 'python'];
+
+//var_dump($programming);
+
+//print_r($programming);
+
+//echo $programming[2];
+
+array_push($programming, 'php','c programming');
+
+echo '<pre>';
+print_r($programming);
+
+echo '</pre>';
