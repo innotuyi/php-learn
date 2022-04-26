108e1efffc8f6ae3207615a51986e31e1d9b9539
root <innotuyi.com>
2022-04-26 09:34:15 +0200

    modified function file

--- function.php
+++ function.php
@@ -1,7 +1,17 @@
 <?php
 
-function hello() {
-    echo 'hello';
+declare(strict_types=1);
+
+// function hello() {
+//     echo 'hello';
+// }
+
+// hello();
+
+function digitOne(int $a)
+{
+
+    return 'hello';
 }
 
-hello();
+var_dump(digitOne(38));
