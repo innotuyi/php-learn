7bc02f1e2f951cafc54f9902e6062eb5518de228
innotuyi <innotuyi67@gmail.com>
2022-05-06 10:43:42 +0200

    Addding namespace file

---
+++ store/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// require 'src/Model/Customer.php';
+
+// $customer = new Customer('Bob');
+// echo $customer->getName();
+
+use Model\Customer;
+
+$customer = new Customer('bob');
+
+echo $customer->getName();
